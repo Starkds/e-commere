166378954a5c089df3fe4a277e072aea266f930f
Starkds <patidarpawan110@gmail.com>
2023-03-25 14:58:52 +0530

    Added more features

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const menuItem = document.querySelectorAll(".menuItem")
     price: 300,
     colors : [
         {
-            code: "black",
+            code: "white",
             img: "./img/My project.png",
         },
         {
@@ -19,32 +19,32 @@ const menuItem = document.querySelectorAll(".menuItem")
 },
 {
     id:2,
-    title:"Air force",
+    title:"Nike",
     price: 190,
     colors : [
         {
-            code: "black",
-            img: "./img/My project(1).png",
+            code: "white",
+            img: "./img/My project (1).png",
         },
         {
-            code:"darkblue",
-            img:"./img/My_project(1).png",
+            code:"yellow",
+            img: "./img/My_project(1).png",
         },
     ],
 },
 
 {
     id:3,
-    title:"Adidas",
-    price: 250,
+    title:"Canvas",
+    price: 370,
     colors : [
         {
             code: "black",
-            img: "./img/My project(2).png",
+            img: "./img/My project (2).png",
         },
         {
-            code:"darkblue",
-            img:"./img/My_project(3).png",
+            code:"gold",
+            img:"./img/My_project(2).png",
         },
     ],
 },
@@ -53,31 +53,31 @@ const menuItem = document.querySelectorAll(".menuItem")
 
 {
     id:4,
-    title:"Nike",
-    price: 800,
+    title:"Adidas",
+    price: 330,
     colors : [
         {
-            code: "black",
-            img: "./img/My project(3).png",
+            code: "red",
+            img: "./img/My project (3).png",
         },
         {
-            code:"darkblue",
-            img:"./img/My_project(3).png",
-        },
+            code:"black",
+            img:"./img/My_projectd(3).png",
+        }, 
     ],
 },
 
 {
     id:5,
-    title:"Jordan",
-    price: 350,
+    title:"Air force",
+    price: 800,
     colors : [
         {
-            code: "black",
-            img: "./img/My project(4).png",
+            code: "purple",
+            img: "./img/My project (4).png",
         },
         {
-            code:"darkblue",
+            code:"light blue",
             img:"./img/My_project(4).png",
         },
     ],
@@ -85,15 +85,15 @@ const menuItem = document.querySelectorAll(".menuItem")
 
 {
     id:6,
-    title:"Redchief",
-    price: 450,
+    title:"Jordan",
+    price: 320,
     colors : [
         {
-            code: "black",
-            img: "./img/My project(5).png",
+            code: "red",
+            img: "./img/My project (5).png",
         },
         {
-            code:"darkblue",
+            code:"green",
             img:"./img/My_project(5).png",
         },
     ],
@@ -122,11 +122,11 @@ menuItem.forEach((item,index) => {
     
 
        //change the choosen product
-       choosenProduct = products[index];
+       choosenProduct = product[index];
 
 
        //change text of currentProduct
-       currentProductTitle.textContent = choosenProduct.Title;
+       currentProductTitle.textContent = choosenProduct.title;
        currentProductPrice.textContent = "$" + choosenProduct.price;
        currentProductImg.src = choosenProduct.colors[0].img 
  
@@ -144,4 +144,26 @@ currentProductColors.forEach((color, index) => {
         currentProductImg.src = choosenProduct.colors[index].img
     }
         )
-})
+});
+
+currentProductSizes.forEach((size,index) => {
+    size.addEventListener("click",() =>{
+        currentProductSizes.forEach(size =>{
+            size.style.backgroundColor= "white";
+            size.style.color = "black";
+        })
+        size.style.backgroundColor= "black";
+        size.style.color = "white";
+    });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+    payment.style.display="flex";
+});
+close.addEventListener("click",()=>{
+    payment.style.display="none";
+});
